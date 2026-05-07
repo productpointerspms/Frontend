38997de8226f9040c7c89b67439ac0bc171cb5d7
omosiki <abdullateefsikirat6@gmail.com>
2026-05-07 19:15:03 +0100

    Commiting the Hero section of PPMP

--- src/components/PPMP/Herosection.tsx
+++ src/components/PPMP/Herosection.tsx
@@ -6,27 +6,19 @@ const Hero = () => {
     // <section className="w-full bg-[#FCF1FF] py-20 px-6 md:px-12 lg:px-20">
      <section className="w-full bg-[#FCF1FF] pt-1 pb-20 px-6 md:px-12 lg:px-20"> 
       <div className="max-w-7xl mx-auto grid md:grid-cols-2  gap-6 items-center">
-
-        {/* LEFT */}
         <div>
-
-          {/* Badge */}
           <span className="inline-block bg-[#FAE1FF] text-purple-700 px-4 py-1 rounded-full text-sm font-medium">
             Beginner-Friendly Program
           </span>
-
-          
-         
-              {/* Heading */}
           {/* <h1 className="mt-6 text-4xl text-[#0C1115] font-extrabold text-gray-600 leading-tight"> */}
-           <h1 className="mt-6 text-4xl font-extrabold text-[#0C1115] leading-tight">
+           <h1 className="mt-6 text-4xl font-montserrat font-extrabold text-[#0C1115] leading-tight">
             Confused About Where <br />
             To Start In Product <br />
             Management?
           </h1>
 
           {/* Description */}
-          <p className="mt-5 text-gray-600 text-lg max-w-lg">
+          <p className="mt-5 text-[#6A6B6C] font-montserrat text-lg max-w-lg">
             The ProductPointers Accelerator Program (PPAP) helps you go from zero
             knowledge to thinking like a Product Manager with structure,
             mentorship, and hands-on learning.
@@ -60,33 +52,23 @@ const Hero = () => {
               />
             </div>
 
-            <p className="text-sm text-gray-600">
-              Join 300+ aspiring Product Managers who started here
+            <p className="text-sm text-[#4B4B4B]">
+              Join <span className="text-black font-bold">300+</span> aspiring Product Managers who started here
             </p>
           </div>
         </div>
 
-        {/* RIGHT IMAGE
-        <div className="w-full flex justify-center">
+
+        {/* RIGHT IMAGE */}
+        <div className="w-full flex justify-end">
           <img
             src="images/ppmp.png"
             alt="PPAP Foundation"
-            className="w-[80%] md:w-full h-auto"
+            className="w-[100%] md:w-full h-auto"
+            
           />
-        </div> */}
-
-        {/* RIGHT IMAGE */}
-  <div className="relative flex justify-end">
-  <Image
-    src="/images/ppmp.png"
-    alt="PPAP Foundation"
-    width={1200}
-    height={1199}
-    className="w-full md:w-[120%] lg:w-[130%] h-auto object-contain"
-  />
-</div>
-
-      </div>
+        </div>
+    </div>
     </section>
   );
 };
